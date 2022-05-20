a1e8b581592f9df651a7a22038f073e95c93a948
txtuan <tuantrinh488@gmail.com>
2022-05-20 17:26:38 +0700

    demo using starter dependencies and automatic configuration

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,34 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "userdata")
+public class User {
+    @Id
+    private int id;
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUname() {
+        return username;
+    }
+
+    public void setUname(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", uname=" + username + "]";
+    }
+}
